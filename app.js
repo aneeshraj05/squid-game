5434c6f34287b7e5e262baf2ffac8723c072a368
aneeshraj05 <aneesh.rajx@gmail.com>
2025-07-26 23:54:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,91 @@
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+// const smoother = ScrollSmoother.create({
+//   wrapper: "#smooth-wrapper",
+//   content: "#smooth-content",
+//   smooth: 1.5,
+//   effects: true,
+//   smoothTouch: 0.1,
+// });
+// const t1=gsap.timeline();
+// t1.from(".left", {
+//   autoAlpha:0,
+// x:-150,
+// skewX:7,
+// duration:1,
+
+
+
+
+// });
+// const t2=gsap.timeline();
+// t1.from(".right", {
+// autoAlpha:0,
+// x:150,
+// skewX:7,
+// duration:1,
+
+
+
+
+// })
+
+// const tl3=gsap.timeline();
+// tl3.from(".img", {
+//   autoAlpha:0,
+//   duration:.5,
+ 
+//   delay:2,
+// })
+// tl3.to("header", {
+//   delay:.5,
+// })
+
+
+
+// const tl4=gsap.timeline({
+//   scrollTrigger:{
+//     trigger:"#section1",
+//     start:"30%",
+//     end:"80%",
+  
+//     scrub:1,
+
+//   }
+  
+// });
+// tl4.to(".img",{
+//     autoAlpha:0,
+   
+//   })
+//   tl4.to(".r",{
+//   autoAlpha:0,
+
+
+// },'a')
+// tl4.to(".l",{
+//   autoAlpha:0,
+
+// },'a')
+//   const tl5=gsap.timeline({
+//   scrollTrigger:{
+//     trigger:"#section1",
+//     start:"10%",
+//     end:"80%",
+
+//     scrub:1,
+
+//   }
+  
+// });
+// tl5.to(".left",{
+//   x:-200,
+//   duration:2,
+//   skewx:3,
+// },'a')
+// tl5.to(".right",{
+//   x:200,
+//   duration:2,
+//   skewX:3,
+//   autoAplha:0,
+// },'a')
